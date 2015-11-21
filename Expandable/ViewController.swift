bd8b5768d77c1f0c0e2d42aae07140afd23f330e
Brian Glowe <brianglowe@gmail.com>
2015-11-21 16:38:55 -0500

    Provided methods to display the descriptions from the cellDescriptors dictionaries in the proper cells

--- Expandable/ViewController.swift
+++ Expandable/ViewController.swift
@@ -75,6 +75,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
     }
     
+    // this function locates and returns the cell description from the cellDescriptors array (the visibleRowsPerSection func is a pre-req for this function)
+    // this function accepts the index path of the cell this is being processed by the TBview as a parameter and returns the properties of that cell
+    func getCellDescriptorForIndexPath(indexPath: NSIndexPath) -> [String: AnyObject] {
+        // the first step is to find the index of the visible row
+        let indexOfVisibleRow = visibleRowsPerSection[indexPath.section][indexPath.row]
+        // next, since we have the index of the row for each cell, we can cast the cell description from the cellDescriptors array to the indexOfVisibleRow aray.
+        let cellDescriptor = cellDescriptors[indexPath.section][indexOfVisibleRow] as! [String: AnyObject]
+        return cellDescriptor
+    }
+    
     // MARK: Custom Functions
     
     func configureTableView() {
@@ -93,27 +103,92 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     // MARK: UITableView Delegate and Datasource Functions
     
+    //this function fixes the existing TBview method to specify the number of sections in the TBView
+    // NOTE - we have to handle if the cellDesciptors is nil
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        if cellDescriptors != nil {
+            return cellDescriptors.count
+        }
+        else {
+            return 0
+        }
     }
     
-    
+    // the number of rows per section is always equal to the number of visible cells
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return visibleRowsPerSection[section].count
     }
     
+    // this function determines the titles for the sections
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Personal"
+            
+        case 1:
+            return "Preferences"
+            
+        default:
+            return "Work Experience"
+        }
+    }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let currentCellDescriptor = getCellDescriptorForIndexPath(indexPath)
+        let cell = tableView.dequeueReusableCellWithIdentifier(currentCellDescriptor["cellIdentifier"] as! String, forIndexPath: indexPath) as! CustomCell
+        
+        // normal cells are the cells with idCellNormal identifier and are the top-level cells with are expanded and collapsed 
+        // normal cells are set with the primaryTitle & secondaryTitle text values
+        if currentCellDescriptor["cellIdentifier"] as! String == "idCellNormal" {
+            if let primaryTitle = currentCellDescriptor["primaryTitle"] {
+                cell.textLabel?.text = primaryTitle as? String
+            }
+            
+            if let secondaryTitle = currentCellDescriptor["secondaryTitle"] {
+                cell.detailTextLabel?.text = secondaryTitle as? String
+            }
+        }
+        // for cells containing a textfield, we set the placeholder value to the primaryTitle property of the cell descriptor
+        else if currentCellDescriptor["cellIdentifier"] as! String == "idCellTextfield" {
+            cell.textField.placeholder = currentCellDescriptor["primaryTitle"] as? String
+        }
+        // switch control cells- first we specifiy the displayed text before the switch & second we set the switch to the proper state (on or off)
+        else if currentCellDescriptor["cellIdentifier"] as! String == "idCellSwitch" {
+            cell.lblSwitchLabel.text = currentCellDescriptor["primaryTitle"] as? String
+            
+            let value = currentCellDescriptor["value"] as? String
+            cell.swMaritalStatus.on = (value == "true") ? true : false
+        }
+        // cells with a picker - provide a list of options and when an option has been selected, the cell will collapse and the value will be displayed in the text label
+        else if currentCellDescriptor["cellIdentifier"] as! String == "idCellValuePicker" {
+            cell.textLabel?.text = currentCellDescriptor["primaryTitle"] as? String
+        }
+        // cells with a slider - first we grab the current value from the currentCellDescriptor dictionary and convert it to a float. We assign the float to the slider control
+        else if currentCellDescriptor["cellIdentifier"] as! String == "idCellSlider" {
+            let value = currentCellDescriptor["value"] as! String
+            cell.slExperienceLevel.value = (value as NSString).floatValue
+        }
         
         return cell
     }
     
-    
+    // this function determines the cell height based on the respective xib files
+    // this is the first use of the getCellDescriptorForIndexPath method declared earlier.
+    // first we need to get the proper cell descriptor, next we need the "cellIdentifier" property which will determine the cell row height.
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 0.0
+        let currentCellDescriptor = getCellDescriptorForIndexPath(indexPath)
+        
+        switch currentCellDescriptor["cellIdentifier"] as! String {
+        case "idCellNormal":
+            return 60.0
+            
+        case "idCellDatePicker":
+            return 270.0
+            
+        default:
+            return 44.0
+        }
     }
-    
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
